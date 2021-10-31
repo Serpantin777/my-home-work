731626c303e8a966166a6d9b971c885fc6ef6c7b
Serpantin777 <arguzin2008@rambler.ru>
2021-10-31 21:46:57 +0500

    5

--- app/src/main/java/com/serpantin/myhomework/MainActivity.kt
+++ app/src/main/java/com/serpantin/myhomework/MainActivity.kt
@@ -2,10 +2,46 @@ package com.serpantin.myhomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val MY_TAG = "MyTag"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(MY_TAG, "onCreate()")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MY_TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MY_TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MY_TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MY_TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MY_TAG, "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MY_TAG, "onDestroy()")
+    }
+
 }
