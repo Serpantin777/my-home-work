a6793d29f57e4d50ec29bb0ee2aabed7dba9c98e
Serpantin777 <arguzin2008@rambler.ru>
2021-11-05 22:49:50 +0500

    7

--- app/src/main/java/com/serpantin/myhomework/MainActivity.kt
+++ app/src/main/java/com/serpantin/myhomework/MainActivity.kt
@@ -2,54 +2,31 @@ package com.serpantin.myhomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 private const val MY_TAG = "MyTag"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity<view> : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(MY_TAG, "1. Как грустны сумрачные дни \n" +
-                "2. Беззвучной осени и хладной!")
+        val showButton: Button = findViewById(R.id.button_end)
+        showButton.setOnClickListener {
+            showSnackbar(it)
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(MY_TAG, "3. Какой истомой безотрадной \n" +
-                "4. К нам в душу просятся они!")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(MY_TAG, "5. Но есть и дни, когда в крови \n" +
-                "6. Золотолиственных уборов")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(MY_TAG, "7. Горящих осень ищет взоров \n" +
-                "8. И знойных прихотей любви.")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(MY_TAG, "9. Молчит стыдливая печаль, \n" +
-                "10. Лишь вызывающее слышно,")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(MY_TAG, "11. И, замирающей так пышно, \n" +
-                "12. Ей ничего уже не жаль.")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(MY_TAG, "11. И, замирающей так пышно, \n" +
-                "12. Ей ничего уже не жаль.")
+    private fun showSnackbar(view: View){
+        Snackbar.make(this, view, "Ничего тут нет!", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Жми!"){
+                Toast.makeText(this, "Good by!", Toast.LENGTH_LONG).show()
+            }
+            .show()
     }
 
 }
---
+++ app/src/test/java/com/serpantin/myhomework/view.kt
@@ -0,0 +1,5 @@
+package com.serpantin.myhomework
+
+class view {
+
+}
