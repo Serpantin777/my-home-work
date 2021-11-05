c3b46fd84c23e3522e9514ae0ea9457d04493c99
Serpantin777 <arguzin2008@rambler.ru>
2021-11-05 17:52:41 +0500

    6

--- app/src/main/java/com/serpantin/myhomework/MainActivity.kt
+++ app/src/main/java/com/serpantin/myhomework/MainActivity.kt
@@ -7,41 +7,49 @@ import android.util.Log
 private const val MY_TAG = "MyTag"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(MY_TAG, "onCreate()")
+        Log.d(MY_TAG, "1. Как грустны сумрачные дни \n" +
+                "2. Беззвучной осени и хладной!")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(MY_TAG, "onStart()")
+        Log.d(MY_TAG, "3. Какой истомой безотрадной \n" +
+                "4. К нам в душу просятся они!")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(MY_TAG, "onResume()")
+        Log.d(MY_TAG, "5. Но есть и дни, когда в крови \n" +
+                "6. Золотолиственных уборов")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(MY_TAG, "onPause()")
+        Log.d(MY_TAG, "7. Горящих осень ищет взоров \n" +
+                "8. И знойных прихотей любви.")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(MY_TAG, "onStop()")
+        Log.d(MY_TAG, "9. Молчит стыдливая печаль, \n" +
+                "10. Лишь вызывающее слышно,")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(MY_TAG, "onRestart()")
+        Log.d(MY_TAG, "11. И, замирающей так пышно, \n" +
+                "12. Ей ничего уже не жаль.")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(MY_TAG, "onDestroy()")
+        Log.d(MY_TAG, "11. И, замирающей так пышно, \n" +
+                "12. Ей ничего уже не жаль.")
     }
 
 }
